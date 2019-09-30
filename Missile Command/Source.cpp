e159c4d81457c078d998e519adf2ef5e09dcb0bc
Allexis Alvarico <C00242855@itcarlow.ie>
2019-09-30 19:49:07 +0100

    The missile selection loop is working and the home base is random generated

--- Missile Command/Source.cpp
+++ Missile Command/Source.cpp
@@ -18,7 +18,7 @@ typedef struct Position
 }Coordinates;
 
 
-enum Warhead { Explosive, Nuclear };
+enum Warhead { Explosive, Nuclear, Failure, Destroyed };
 
 typedef struct Enemy
 {
@@ -36,17 +36,18 @@ struct missileCommand
 
 	bool m_armed;
 
-	void arm()
-	{
-		if (arm)
-		{
-			m_armed = false;
-		}
-		else
-		{
-			m_armed = true;
-		}
-	}
+
+	//void arm()
+	//{
+	//	if (arm)
+	//	{
+	//		m_armed = false;
+	//	}
+	//	else
+	//	{
+	//		m_armed = true;
+	//	}
+	//}
 
 
 	void update()
@@ -61,9 +62,55 @@ int main()
 {
 
 
+	
+	srand(time(NULL));
 
+	Position base;
 
-	std::cout << "Select a Warhead: ";
+	base.m_x = rand() % 20 + 1;
+	base.m_y = rand() % 20 + 1;
+
+	missileCommand bomb;
+
+	Enemy division;
+
+	division.coordinates.m_x = rand() % 20 + 1;
+	division.coordinates.m_y = rand() % 20 + 1;
+
+	bool m_gameover = false;
+	bool m_misssileSelect = true;
+	int m_selection;
+	int m_nuke = 1;
+	int m_bomb = 2;
+
+	while (!m_gameover)
+	{
+		if (m_misssileSelect)
+		{
+			std::cout << "Your home base is X:" << base.m_x << " Y:" << base.m_y << "\n";
+			std::cout << "Nuke = 1\nBomb = 2\nSelect a Warhead: ";
+			std::cin >> m_selection;
+			if (m_selection == m_nuke)
+			{
+				std::cout << "You have selected the Nuke\n";
+				bomb.payload = Warhead::Nuclear;
+				m_misssileSelect = false;
+				break;
+			}
+			else if (m_selection == m_bomb)
+			{
+				std::cout << "You have selected the Bomb\n";
+				bomb.payload = Warhead::Explosive;
+				m_misssileSelect = false;
+				break;
+			}
+			else
+			{
+				std::cout << "The missile you selected is not in the armorary\n";
+			}
+		}	
+	}
+
 
 	std::cout << "Looking for targets: ";
 
@@ -71,12 +118,11 @@ int main()
 
 	std::cout << "You have hit the target!! ";
 
+	std::cout << "You have destroyed the target";
+
 	std::cout << "You have missed the target!! ";
-
-
-
-
-
+	
+	bomb.update();
 
 
 	system("PAUSE");
