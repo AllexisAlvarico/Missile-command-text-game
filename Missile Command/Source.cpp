50d4bf027f7f36944704e5ed8d9cba95a43daddf
Allexis Alvarico <C00242855@itcarlow.ie>
2019-09-30 22:48:07 +0100

    Added the launching input, targeting code and looping

--- Missile Command/Source.cpp
+++ Missile Command/Source.cpp
@@ -61,7 +61,7 @@ struct missileCommand
 int main()
 {
 
-
+	
 	
 	srand(time(NULL));
 
@@ -74,53 +74,134 @@ int main()
 
 	Enemy division;
 
-	division.coordinates.m_x = rand() % 20 + 1;
-	division.coordinates.m_y = rand() % 20 + 1;
 
 	bool m_gameover = false;
 	bool m_misssileSelect = true;
+	bool m_target = false;
+	bool m_missileLaunch = false;
 	int m_selection;
-	int m_nuke = 1;
-	int m_bomb = 2;
+	int m_chanceOfHit;
+	int m_nuclear = 8;
+	int m_ballistic = 20;
+	int m_enemy = 10;
+	int m_destroyed = 0;
+	int m_disabled = 0;
 
 	while (!m_gameover)
 	{
 		if (m_misssileSelect)
 		{
-			std::cout << "Your home base is X:" << base.m_x << " Y:" << base.m_y << "\n";
-			std::cout << "Nuke = 1\nBomb = 2\nSelect a Warhead: ";
+
+
+			std::cout << "---------------------------\nYour home base is X:" << base.m_x << " Y:" << base.m_y <<
+				"\nArmory:\nNuclear missile " << m_nuclear << "\nBallistic missile: " << m_ballistic
+				<< "\n---------------------------\nNuclear Missile = 1 Ballistic Missiles= 2\n---------------------------\nSelect a Warhead: ";
 			std::cin >> m_selection;
-			if (m_selection == m_nuke)
+			std::cout << "---------------------------\n";
+			if (m_selection == 1)
 			{
-				std::cout << "You have selected the Nuke\n";
-				bomb.payload = Warhead::Nuclear;
-				m_misssileSelect = false;
-				break;
+				if (m_nuclear != 0)
+				{
+					std::cout << "You have selected the nuclear missile\n";
+					bomb.payload = Warhead::Nuclear;
+					m_target = true;
+					m_nuclear--;
+					m_misssileSelect = false;
+				}
 			}
-			else if (m_selection == m_bomb)
+			else if (m_selection == 2)
 			{
-				std::cout << "You have selected the Bomb\n";
+				std::cout << "You have selected the ballistic missile\n";
 				bomb.payload = Warhead::Explosive;
+				m_ballistic--;
+				m_target = true;
 				m_misssileSelect = false;
-				break;
 			}
 			else
 			{
-				std::cout << "The missile you selected is not in the armorary\n";
+				std::cout << "The missile you selected is not in the armory\n";
 			}
-		}	
+		}
+		if (m_target)
+		{
+
+			division.coordinates.m_x = rand() % 20 + 1;
+			division.coordinates.m_y = rand() % 20 + 1;
+
+			std::cout << "---------------------------\nScaning for targets...\n Found multiple contacts: " << m_enemy << "\n---------------------------\nTargeting enemy on X:" << division.coordinates.m_x << " Y:" << division.coordinates.m_y << " coordinates\n";
+			std::cout << "You have found tank\nConfirm to launch missile?\nYes = 1 or No = 2\n---------------------------\n";
+			std::cin >> m_selection;
+
+			if (m_selection == 1)
+			{
+				std::cout << "---------------------------\nLaunching missile\n";
+				m_missileLaunch = true;
+				m_target = false;
+
+			}
+			else if (m_selection == 2)
+			{
+				std::cout << "Aborting missile launch";
+				m_misssileSelect = true;
+			}
+			else
+			{
+				std::cout << "---------------------------\nYour selection is not vaild, canceling launch\n";
+				if (bomb.payload == Warhead::Nuclear)
+				{
+					m_nuclear++;
+				}
+				else if (bomb.payload == Warhead::Explosive)
+				{
+					m_ballistic++;
+				}
+
+				m_misssileSelect = true;
+				m_target = false;
+			}
+		}
+
+		if (m_missileLaunch)
+		{
+			std::cout << "The missile has launched standby......\n";
+
+			m_chanceOfHit = rand() % 10 + 1;
+
+			if (bomb.payload == Warhead::Nuclear)
+			{
+				if (m_chanceOfHit >= 3)
+				{
+					std::cout << "The Nuclear Missile has splashed at the target and is destroyed\n";
+					m_enemy--;
+					m_destroyed++;
+					m_misssileSelect = true;
+				}
+				else
+				{
+					std::cout << "The Nuclear Missile has failed to connect on target but the target is disabled\n";
+					m_enemy--;
+					m_disabled++;
+					m_misssileSelect = true;
+				}
+			}
+			if (bomb.payload == Warhead::Explosive)
+			{
+				if (m_chanceOfHit >= 5)
+				{
+					std::cout << "Ballistic missile has splashed at the target and is destroyed\n";
+					m_enemy--;
+					m_misssileSelect = true;
+				}
+				else
+				{
+					std::cout << "Ballistic missile has failed to connect on target\n";
+					m_misssileSelect = true;
+				}
+			}
+
+		}
+
 	}
-
-
-	std::cout << "Looking for targets: ";
-
-	std::cout << "Select the Coordinates: ";
-
-	std::cout << "You have hit the target!! ";
-
-	std::cout << "You have destroyed the target";
-
-	std::cout << "You have missed the target!! ";
 	
 	bomb.update();
 
